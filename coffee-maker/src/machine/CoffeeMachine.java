f57c49892dfedf8279d6d4b527c548a7485ab16a
Phong Van Nguyen <phong_nguyen@hcl.com>
2020-12-23 21:20:59 -0500

    Step 4: refactor code, make methods smaller

--- coffee-maker/src/machine/CoffeeMachine.java
+++ coffee-maker/src/machine/CoffeeMachine.java
@@ -4,34 +4,111 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
 	
-	static int getMaxCupOfCoffee(int water, int milk, int bean) {
-		int maxCupOfCoffee = Math.min(Math.min(water/200, milk/50), bean/15);		
-		return maxCupOfCoffee;
+	private int water;
+	private int milk;
+	private int bean;
+	private int cup;
+	private int money;
+	
+	public CoffeeMachine() {
+		this.water = 400;
+		this.milk = 540;
+		this.bean = 120;
+		this.cup = 9;
+		this.money = 550;
+	}
+	
+	public void buy(int option) {
+		switch(option) {
+		case 1:
+			buyOneEspresso();
+			break;
+		case 2:
+			buyOneLatte();
+			break;
+		case 3:
+			BuyOneCappuccino();
+			break;
+		}
+		
+		showStatistics();
+	}
+	
+	private void buyOneEspresso() {
+		subtractInventory(250, 0, 16);
+		this.money += 4;
+	}
+	
+	private void buyOneLatte() {
+		subtractInventory(350, 75, 20);
+		this.money += 7;
+	}
+	
+	private void BuyOneCappuccino() {
+		subtractInventory(200, 100, 12);
+		this.money += 6;
+	}
+	
+	private void subtractInventory(int water, int milk, int bean) {
+		this.water -= water;
+		this.milk -= milk;
+		this.bean -= bean;
+		this.cup -= 1;
+	}
+	
+	public void fill (int water, int milk, int bean, int cup) {
+		this.water += water;
+		this.milk += milk;
+		this.bean += bean;
+		this.cup += cup;
+		
+		showStatistics();
+	}
+	
+	public void take () {
+		System.out.printf("I gave you $%d\n", this.money);
+		this.money = 0;
+		
+		System.out.println();
+		showStatistics();
+	}
+	
+	public void showStatistics() {
+		System.out.println("The coffee machine has:");
+		System.out.printf("%d of water\n", this.water);
+		System.out.printf("%d of milk\n", this.milk);
+		System.out.printf("%d of coffee beans\n", this.bean);
+		System.out.printf("%d of disposable cups\n", this.cup);
+		System.out.printf("%d of money\n", this.money);
 	}
 	
     public static void main(String[] args) {
+        CoffeeMachine cfm = new CoffeeMachine();
+        cfm.showStatistics();
+        
         Scanner scanner = new Scanner(System.in);
+        System.out.println("\nWrite action (buy, fill, take):");
+        String choice = scanner.nextLine();
         
-        System.out.println("Write how many ml of water the coffee machine has:");
-        int water = scanner.nextInt();
-        
-        System.out.println("Write how many ml of milk the coffee machine has:");
-        int milk = scanner.nextInt();
-        
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        int bean = scanner.nextInt();
-        
-        System.out.println("Write how many cups of coffee you will need:");
-        int cupOfCoffee = scanner.nextInt();
-        
-        int maxCupOfCoffee = getMaxCupOfCoffee(water, milk, bean);
-        if (cupOfCoffee == maxCupOfCoffee) {
-        	System.out.println("Yes, I can make that amount of coffee");
-        } else if (cupOfCoffee < maxCupOfCoffee) {
-        	System.out.printf("Yes, I can make that amount of coffee "
-        					+ "(and even %d more than that)\n", (maxCupOfCoffee - cupOfCoffee));
+        if (choice.equals("buy")) {
+        	System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+        	int coffeeChoice = scanner.nextInt();
+        	
+        	System.out.println();
+        	cfm.buy(coffeeChoice);
+        } else if (choice.equals("fill")) {
+        	System.out.println("Write how many ml of water do you want to add:");
+        	int water = scanner.nextInt();
+        	System.out.println("Write how many ml of milk do you want to add:");
+        	int milk = scanner.nextInt();
+        	System.out.println("Write how many grams of coffee beans do you want to add:");
+        	int bean = scanner.nextInt();
+        	System.out.println("Write how many disposable cups of coffee do you want to add:");
+        	int coffee = scanner.nextInt();
+        	
+        	cfm.fill(water, milk, bean, coffee);
         } else {
-        	System.out.printf("No, I can make only %d cup(s) of coffee", maxCupOfCoffee);
+        	cfm.take();
         }
     }
 }
